6d1f33653a7820b5a3e419a81509b04d72e0ec94
Rubanagakene Robinson <97873751+wongiro@users.noreply.github.com>
2022-10-01 05:09:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var todoList = document.getElementById('todoList');
+var additems = document.getElementById('addTodo');
+additems.onclick = function(){
+    var todo_obj = document.getElementById("todo").value;
+    
+    if (todo_obj !== ""){
+
+        var li = document.createElement('li');
+        li.innerHTML = todo_obj;
+        todoList.appendChild(li);
+        document.getElementById("todo").value = "";
+
+    }
+}
